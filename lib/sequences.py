48784fc95c2b5d4b7af2572e80c8f2c6f813bc69
egalosman <ahmedmoha1500@gmail.com>
2024-09-25 13:41:15 +0300

    assignment done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+            print([])
+    elif length == 1:
+         print([0])
+    elif length == 2:
+         print([0,1])
+    else:
+        a,b = 0,1
+        fib_list = [0,1]
+        for x in range(length - 2):
+            c = a + b
+            a = b
+            b = c
+            fib_list.append(c)
+
+        
+        print(fib_list)
+        return fib_list
+
+
+print(print_fibonacci(10))
+
